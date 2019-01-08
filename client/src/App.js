c747cf3633110287dae67bb44f35313e81348267
bevrivling <bev.rivling@gmail.com>
2019-01-08 12:59:13 +0000

    added React functionality

---
+++ client/src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import "./App.css";
+import ReadString from "./ReadString";
+import SetString from "./SetString";
+
+class App extends Component {
+  state = { loading: true, drizzleState: null };
+
+  render() {
+    if (this.state.loading) return "Loading Drizzle...";
+    return (
+      <div className="App">
+        <ReadString
+          drizzle={this.props.drizzle}
+          drizzleState={this.state.drizzleState}
+        />
+        <SetString
+          drizzle={this.props.drizzle}
+          drizzleState={this.state.drizzleState}
+        />
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    const { drizzle } = this.props;
+
+    this.unsubscribe = drizzle.store.subscribe(() => {
+      const drizzleState = drizzle.store.getState();
+
+      if (drizzleState.drizzleStatus.initialized) {
+        this.setState({ loading: false, drizzleState });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+}
+
+export default App;
--- contracts/MyStringStore.sol
+++ contracts/MyStringStore.sol
@@ -1,9 +1,9 @@
 pragma solidity ^0.5.0;
 
 contract MyStringStore {
-  string public myString = "Hello World";
+    string public myString = "Hello World";
 
-  function set(string x) public {
-    myString = x;
-  }
+    function set(string memory x) public {
+        myString = x;
+    }
 }
